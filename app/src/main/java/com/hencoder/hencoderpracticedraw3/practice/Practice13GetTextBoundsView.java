4d7acd23130dafc78597de3cbfeeb1e7c36934b7
liuxiaofeng8888 <liuxiaofeng54222@sina.com>
2017-11-24 17:47:15 +0800

    hencode3

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -44,17 +45,20 @@ public class Practice13GetTextBoundsView extends View {
         super.onDraw(canvas);
 
         canvas.drawRect(50, top, getWidth() - 50, bottom, paint1);
-
         // 使用 Paint.getTextBounds() 计算出文字的显示区域
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
-
+        Rect rect = new Rect();
+        paint2.getTextBounds(text1, 0, text1.length(), rect);
+        int offset1 = (rect.top+rect.bottom)/2;
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        canvas.drawText(text1, 100, middle+offset1, paint2);
+        canvas.drawText(text2, 200, middle+offset1, paint2);
+        canvas.drawText(text3, 300, middle+offset1, paint2);
+        canvas.drawText(text4, 400, middle+offset1, paint2);
+        canvas.drawText(text5, 500, middle+offset1, paint2);
+        canvas.drawText(text6, 600, middle+offset1, paint2);
+
+
     }
 }
